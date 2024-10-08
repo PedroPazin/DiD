f6042769f45cceb4e058d4b1258c6e33be1daeab
Pedro Lucas Ferreira Pazin <129557278+PedroPazin@users.noreply.github.com>
2024-10-08 11:16:30 -0300

    Update Main.java

--- DiD-main/DiD/src/Main.java
+++ DiD-main/DiD/src/Main.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void chooseClass(){
+    public static void showClasses(){
         System.out.println("Escolha sua classe");
 
+        // For que vai mostrar todas as classes na tela
         int i = 1;
         for(Classes c: Classes.values()){
             System.out.print(i + " -> " + c + " [");
-            c.getClasse().getStats();
+            c.getClasse().getStats(); // Mostrara todos os status da classe
             System.out.println("]");
             i++;
         }
-
 
     }
 
@@ -26,7 +26,24 @@ public class Main {
         Scanner obj = new Scanner(System.in);
 
         System.out.println("=-=-=-=-=-=- Bem vindo Jogador -=-=-=-=-=-=");
-        chooseClass(); // Metodo para escolher a classe
+        showClasses(); // Metodo para mostrar as classes
+        String classe; // String que recebera o index da classe
+        List<String> idxClasse = new ArrayList<>(Arrays.asList("1", "2", "3")); // Lista com os valores validos do index de classe
+        // Loop infinito para o jogador escolher sua classe
+        while(true){
+            classe = obj.next().trim(); // Input da classe retirando os espaços vazios
+
+            if(idxClasse.contains(classe)){ // Se o input conter algum item da lista, esta valido, entao saira do loop
+               break;
+            }
+
+            // Caso o jogador digite um numero invalido
+            System.out.println("Digite um numero de classe válido");
+        }
+
+        // Guardando a classe em uma variavel
+        Classes classeJogador = Classes.values()[Integer.parseInt(classe)-1];
+
 
     }
 }
